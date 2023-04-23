b7f4750a6dd564325f6eaf10a1870383ba07e27d
hoonn9 <hun9935@gmail.com>
2023-04-23 20:36:19 +0900

    feat(DateColumnEntity): swagger ApiProperty 옵션 정의

--- backend/src/common/entity/date-column.entity.ts
+++ backend/src/common/entity/date-column.entity.ts
@@ -1,12 +1,22 @@
 import { CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from 'typeorm';
+import { ApiDateProperty } from '../lib/swagger/decorator/api-date-property.decorator';
 
 export abstract class DateColumnEntity {
+  @ApiDateProperty({
+    name: 'createDate',
+    description: '생성된 시간입니다.',
+  })
   @CreateDateColumn({
     type: 'timestamp with time zone',
     name: 'create_date',
   })
   readonly createDate: Date;
 
+  @ApiDateProperty({
+    name: 'updateDate',
+    isOptional: true,
+    description: '데이터가 갱신된 시간입니다.',
+  })
   @UpdateDateColumn({
     type: 'timestamp with time zone',
     name: 'update_date',
