7c134036059cb6ce381236bc18430a17c1a82f50
hoonn9 <hun9935@gmail.com>
2023-04-28 22:14:12 +0900

    test(CompanyQueryService): findOneByIdOrFail 테스트 추가

--- backend/test/lib/mock/mock-typeorm.ts
+++ backend/test/lib/mock/mock-typeorm.ts
@@ -1,4 +1,9 @@
-import { ColumnOptions, EntityManager } from 'typeorm';
+import {
+  ColumnOptions,
+  EntityManager,
+  ObjectLiteral,
+  Repository,
+} from 'typeorm';
 import { mockAllFields } from './util';
 
 export const MockTypeormDateColumns = () => {
@@ -25,10 +30,14 @@ export const MockTypeormDateColumns = () => {
 
 export const mockEntityManager = () => mockAllFields(EntityManager);
 
-export const mockRepository = () => ({
-  createQueryBuilder: jest.fn().mockReturnValue({
-    where: jest.fn().mockReturnThis(),
-    getOne: jest.fn().mockReturnThis(),
-    getExists: jest.fn().mockReturnThis(),
-  }),
-});
+export const mockRepository = <T extends ObjectLiteral>(): Repository<T> => {
+  const result = {
+    createQueryBuilder: jest.fn().mockReturnValue({
+      where: jest.fn().mockReturnThis(),
+      getOne: jest.fn().mockReturnThis(),
+      getExists: jest.fn().mockReturnThis(),
+    }),
+  } as unknown as Repository<T>;
+
+  return result;
+};
