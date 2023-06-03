770593f4c41ff1d6c1e792386b0be675984ad84d
hoonn9 <hun9935@gmail.com>
2023-06-03 23:14:20 +0900

    refactor(Post): CreateMemberPost Swagger 설정

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,7 @@
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UseController } from '../common/decorator/use-controller.decorator';
 import { PostService } from './post.service';
-import { Body, Post } from '@nestjs/common';
+import { Body, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { TransactionContext } from '../common/decorator/transaction-manager.decorator';
 import { TransactionRoute } from '../common/decorator/transaction-route.decorator';
 import { Auth } from '../auth/decorator/auth.decorator';
@@ -9,12 +9,26 @@ import { CurrentUser } from '../auth/decorator/current-user.decorator';
 import { MemberEntity } from '../member/entity/member.entity';
 import { TransactionManager } from '../common/type/transaction-manager.type';
 import { CreateMemberPostBodyDto } from './dto/create-member-post.dto';
+import { ApiHttpResponse } from '../common/lib/swagger/decorator/api-http-response.decorator';
+import { ApiHttpExceptionResponse } from '../common/lib/swagger/decorator/api-http-exception-response.decorator';
 
 @ApiTags('post')
 @UseController('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  @ApiOperation({
+    description: '게시글을 등록합니다.',
+  })
+  @ApiHttpResponse(HttpStatus.CREATED, [
+    {
+      title: '게시글 등록에 성공했을 경우',
+      description: '게시글이 성공적으로 생성되었을 때의 응답입니다.',
+      type: true,
+    },
+  ])
+  @ApiHttpExceptionResponse(HttpStatus.BAD_REQUEST)
+  @HttpCode(HttpStatus.CREATED)
   @Auth()
   @TransactionRoute()
   @Post()
